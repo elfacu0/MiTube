a11bbd363051b3c1024276e765d60fe654f662a9
k- <mahat.mejor@gmail.com>
2020-05-11 10:07:43 -0500

    Updates

--- precache-manifest.47e2a436930bbb830da02aa26041ffb0.js
+++ precache-manifest.47e2a436930bbb830da02aa26041ffb0.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4ad56bd03a0eae9bdd6690ce77c77f6d",
+    "revision": "e978161fcdbda0013b08555a365307e5",
     "url": "/MyTube/index.html"
   },
   {
-    "revision": "0b9d72aba2b301b324e7",
+    "revision": "30ea6a7086d83523eee4",
     "url": "/MyTube/static/css/main.f7c0852f.chunk.css"
   },
   {
-    "revision": "30332ecab0de889cb1e8",
-    "url": "/MyTube/static/js/2.8d942843.chunk.js"
+    "revision": "31eebc2e1c88f2a4a38c",
+    "url": "/MyTube/static/js/2.fc1dc20d.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/MyTube/static/js/2.8d942843.chunk.js.LICENSE.txt"
+    "url": "/MyTube/static/js/2.fc1dc20d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0b9d72aba2b301b324e7",
-    "url": "/MyTube/static/js/main.2e010ddd.chunk.js"
+    "revision": "30ea6a7086d83523eee4",
+    "url": "/MyTube/static/js/main.e76336a7.chunk.js"
   },
   {
     "revision": "ad34825055fd49b4f2f9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MyTube/precache-manifest.2fca163bc27e49faba6c97d837948736.js"
+  "/MyTube/precache-manifest.47e2a436930bbb830da02aa26041ffb0.js"
 );
 
 self.addEventListener('message', (event) => {
