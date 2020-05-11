59a83cbc741ee15d42ad12b48a079cbcdfe131db
k- <mahat.mejor@gmail.com>
2020-05-11 07:20:18 -0500

    Updates

---
+++ precache-manifest.2fca163bc27e49faba6c97d837948736.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad56bd03a0eae9bdd6690ce77c77f6d",
+    "url": "/MyTube/index.html"
+  },
+  {
+    "revision": "0b9d72aba2b301b324e7",
+    "url": "/MyTube/static/css/main.f7c0852f.chunk.css"
+  },
+  {
+    "revision": "30332ecab0de889cb1e8",
+    "url": "/MyTube/static/js/2.8d942843.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyTube/static/js/2.8d942843.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b9d72aba2b301b324e7",
+    "url": "/MyTube/static/js/main.2e010ddd.chunk.js"
+  },
+  {
+    "revision": "ad34825055fd49b4f2f9",
+    "url": "/MyTube/static/js/runtime-main.4ef3af60.js"
+  }
+]);
